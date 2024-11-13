f7aca3c7de49b065e3c8ed0678696a04a8b6b798
iHeyd <0_iheyd_0@mail.ru>
2024-11-14 01:03:56 +0700

    1.0

---
+++ EducationSQL/MainForm.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace EducationSQL
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.runButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.diagramButton = new System.Windows.Forms.Button();
+            this.contentButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // runButton
+            // 
+            this.runButton.BackColor = System.Drawing.Color.Transparent;
+            this.runButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("runButton.BackgroundImage")));
+            this.runButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.runButton.FlatAppearance.BorderSize = 0;
+            this.runButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.runButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.runButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.runButton.Location = new System.Drawing.Point(777, 648);
+            this.runButton.Margin = new System.Windows.Forms.Padding(0);
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(156, 30);
+            this.runButton.TabIndex = 0;
+            this.runButton.UseVisualStyleBackColor = false;
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Transparent;
+            this.clearButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("clearButton.BackgroundImage")));
+            this.clearButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.clearButton.FlatAppearance.BorderSize = 0;
+            this.clearButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.clearButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.clearButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clearButton.Location = new System.Drawing.Point(938, 648);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(0);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(156, 30);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.UseVisualStyleBackColor = false;
+            // 
+            // exportButton
+            // 
+            this.exportButton.BackColor = System.Drawing.Color.Transparent;
+            this.exportButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("exportButton.BackgroundImage")));
+            this.exportButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.exportButton.FlatAppearance.BorderSize = 0;
+            this.exportButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.exportButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.exportButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exportButton.Location = new System.Drawing.Point(1098, 648);
+            this.exportButton.Margin = new System.Windows.Forms.Padding(0);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(156, 30);
+            this.exportButton.TabIndex = 2;
+            this.exportButton.UseVisualStyleBackColor = false;
+            // 
+            // diagramButton
+            // 
+            this.diagramButton.BackColor = System.Drawing.Color.Transparent;
+            this.diagramButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("diagramButton.BackgroundImage")));
+            this.diagramButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.diagramButton.FlatAppearance.BorderSize = 0;
+            this.diagramButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.diagramButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.diagramButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.diagramButton.Location = new System.Drawing.Point(330, 648);
+            this.diagramButton.Margin = new System.Windows.Forms.Padding(0);
+            this.diagramButton.Name = "diagramButton";
+            this.diagramButton.Size = new System.Drawing.Size(168, 30);
+            this.diagramButton.TabIndex = 3;
+            this.diagramButton.UseVisualStyleBackColor = false;
+            // 
+            // contentButton
+            // 
+            this.contentButton.BackColor = System.Drawing.Color.Transparent;
+            this.contentButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("contentButton.BackgroundImage")));
+            this.contentButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.contentButton.FlatAppearance.BorderSize = 0;
+            this.contentButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.contentButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.contentButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.contentButton.Location = new System.Drawing.Point(556, 648);
+            this.contentButton.Margin = new System.Windows.Forms.Padding(0);
+            this.contentButton.Name = "contentButton";
+            this.contentButton.Size = new System.Drawing.Size(168, 30);
+            this.contentButton.TabIndex = 4;
+            this.contentButton.UseVisualStyleBackColor = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.contentButton);
+            this.Controls.Add(this.diagramButton);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.runButton);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "MainForm";
+            this.Text = "EducationSQL";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button contentButton;
+
+        private System.Windows.Forms.Button diagramButton;
+
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exportButton;
+
+        private System.Windows.Forms.Button runButton;
+
+        #endregion
+    }
+}
